6c55111b2303d8f65791ef6b2025e3bbd018567f
Jaichyi <84934225+JaichyiChen@users.noreply.github.com>
2022-05-13 16:55:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
+const fs = require("fs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // setup static and middleware
 app.use(express.static(__dirname + "/public"));
@@ -9,16 +11,25 @@ app.use(express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./main.html"));
 });
-app.get("/main.html", (req, res) => {
+app.get("/main", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./main.html"));
 });
-app.get("/about.html", (req, res) => {
+app.get("/about", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./about.html"));
 });
-app.get("/services.html", (req, res) => {
+app.get("/services", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./services.html"));
 });
-app.get("/contact.html", (req, res) => {
+app.get("/contact", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./contact.html"));
+});
+// post request for writing into the file
+app.post("/contact", (req, res) => {
+  fs.writeFile("output.txt", req.body.text, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
   res.sendFile(path.resolve(__dirname, "./contact.html"));
 });
 app.all("*", (req, res) => {
